a394200c950e78b440d508a91d530308737c3863
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2021-07-16 20:15:12 +0000

    Deploying to gh-pages from @ Vadym-Hryshchuk/team-project-icecream@33a5c48256a58f0fd13a2e4a8d98b635ae150da3 🚀

--- src.c3c6ec2c.js
+++ src.c3c6ec2c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-icecream/src.bbbf66db.js.map
+//# sourceMappingURL=/team-project-icecream/src.c3c6ec2c.js.map
